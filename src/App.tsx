3fb80b9dd7ab502f96ceb055de08a22fb40a6b14
abhishek17551 <ababhishek113@gmail.com>
2024-02-09 00:19:57 +0530

    created skeletal components and included routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 import './global.css'
+import SigninForm from './_auth/forms/SigninForm'
+
+import SignupForm from './_auth/forms/SignupForm'
+import { Home } from './_root/pages'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
 
 function App() {
 
   return (
-    
-      <p className='text-3xl bg-black text-green-400'>Om Ganeshaya Namaha _/\_</p>
+    <main className='flex h-screen'>
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout/>}>
+          <Route path='/sign-in' element={<SigninForm/>}/>
+          <Route path='/sign-up' element={<SignupForm/>}/>
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout/>}>
+          <Route index element={<Home/>}/>
+        </Route>
+      </Routes>
+    </main>
     
   )
 }
