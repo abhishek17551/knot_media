7dd1351a7b533611fdbf25e9c11f78b9790978db
abhishek17551 <ababhishek113@gmail.com>
2024-02-08 23:44:24 +0530

    installed & configured vite,tailwind

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from '../public/assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      Om Ganeshaya Namaha _/\_
+    </>
+  )
+}
+
+export default App
