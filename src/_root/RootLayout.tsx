c43f83a077ec6a5a869c0abccb5bc0200013f8bc
abhishek17551 <ababhishek113@gmail.com>
2024-02-20 20:35:48 +0530

    feat:created Navbar component with Sign out functionality

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,8 +1,21 @@
+import Footer from '@/components/shared/Footer'
+import Navbar from '@/components/shared/Navbar'
+import Sidebar from '@/components/shared/Sidebar'
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <div className='w-full md:flex'>
+      <Navbar/>
+      <Sidebar/>
+
+      <section className='flex flex-1 h-full'>
+          <Outlet/>
+      </section>
+
+      <Footer/>
+    </div>
   )
 }
 
